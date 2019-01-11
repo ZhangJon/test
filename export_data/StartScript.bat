29c30004ded82b655c425f64d03ed44015170612
Administrator <375686640@qq.com>
2019-01-11 17:34:33 +0800

    201901111734

---
+++ export_data/StartScript.bat
@@ -0,0 +1,5 @@
+﻿echo "======START======" >> D:\Scripts\Script\log\export_data.log
+PowerShell -Command "get-date" >> D:\Scripts\Script\log\export_data.log
+python D:\Scripts\Script\export_data\APP_Q_DATA
+PowerShell -Command "get-date" >> D:\Scripts\Script\log\export_data.log
+echo "======END======" >> D:\Scripts\Script\log\export_data.log
