982eb84475c930b4c3c68cbb0f8399e2e462bada
Administrator <375686640@qq.com>
2019-01-17 13:52:17 +0800

    Update structure.py

--- structure.py
+++ structure.py
@@ -126,8 +126,6 @@ import base64
 from cryptography.hazmat.primitives import padding
 from cryptography.hazmat.primitives.ciphers import Cipher, algorithms, modes
 from cryptography.hazmat.backends import default_backend
-
-
 class AESCrypto(object):
     """AESCrypto."""
 
@@ -234,7 +232,8 @@ class AESCrypto(object):
             return uppadded_data
 
 # message = "QWERTYgfdsa12345！·#￥%"
-message = '123456'
+# message = '123456'
+message = 'LIS5152QRY'
 # message = '\x82\x88\x10Q,\xfe\xdcS \xce\x9e\x95\x98,\xffP'
 # crypto = AESCrypto('abcd1111abcd1111abcd1111abcd1111', 'abcd1111abcd1111')
 crypto = AESCrypto('张-密^&($#@-7/*FK-ybf码w-学f', 'm2Vd=G进yt%爱&')
